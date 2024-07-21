9d27bbbd8005a754f25668536dc47e0da34d4497
Mihai Cojocaru <mihaicojocaru@Mihais-MacBook-Air.local>
2024-07-21 10:19:55 +0300

    Entities, Repository & Models

---
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -0,0 +1,31 @@
+import 'package:news_app/features/daily_news/data/models/article.dart';
+
+import '../../../../core/resources/data_state.dart';
+import '../../domain/entities/article.dart';
+import '../../domain/repository/article_repository.dart';
+
+class ArticleRepositoryImpl implements ArticleRepository {
+  @override
+  Future<DataState<List<ArticleModel>>> getNewsArticles() {
+    // TODO: implement getNewsArticles
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ArticleEntity>> getSavedArticles() {
+    // TODO: implement getSavedArticles
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> removeArticle(ArticleEntity article) {
+    // TODO: implement removeArticle
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> saveArticle(ArticleEntity article) {
+    // TODO: implement saveArticle
+    throw UnimplementedError();
+  }
+}
